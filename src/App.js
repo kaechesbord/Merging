faeb357b0a261772cda5bd83cf0cfa8c9772500f
kaechesbord <98842795+kaechesbord@users.noreply.github.com>
2023-01-17 19:24:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import Preloader from "./components/Preloader";
 import Header from "./components/Header";
 import TodoInput from "./components/TodoInput";
 
-
-
-
 function App() {
   const [tasks, setTasks] = useState([]);
 
@@ -26,16 +23,29 @@ function App() {
     });
     setTasks(res.data);
   };
-  
+  const deleteTodo = async (id) => {
+    try{
+      await axios.delete(`http://localhost:3000/todos/${id}`);
+      setTasks(
+        tasks.filter((task) => {
+          return task.id !== id;
+        })
+      );
+    }
+    catch{
+      alert("NOT GOOD")
+    }
+  };
   return (
     <div className="todoapp stack-large">
       <div className="container">
         <Header />
-        <TodoInput createTodo={createTodo}/>
-        {tasks ? <Todo tasks={tasks} /> : <Preloader />}
+        <TodoInput createTodo={createTodo} />
+        {tasks ? <Todo tasks={tasks} deleteTodo={deleteTodo} /> : <Preloader />}
       </div>
     </div>
   );
 }
 
 export default App;
+
