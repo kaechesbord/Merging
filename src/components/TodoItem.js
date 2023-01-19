3ff26c8c48ec832f644c7db26ae0fc0d64b8d9e1
kaechesbord <98842795+kaechesbord@users.noreply.github.com>
2023-01-19 19:24:44 +0100

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./TodoItem.css";
 
-const TodoItem = ({ task, deleteTodo }) => {
-  
-  const deleteTodoHandler = () => {
-    deleteTodo(task.id)
-  }
+const TodoItem = ({ task, deleteTodo, openModal }) => {
+  const [isCompleted, setIsCompleted] = useState(false);
 
+  const deleteTodoHandler = () => {
+    deleteTodo(task.id);
+  };
+  const handleCompletion = () => {
+    if (isCompleted) {
+      setIsCompleted(false);
+    } else {
+      setIsCompleted(true);
+    }
+  };
+  const classNameIsCompleted = async() => {
+    await axios.put(`http://localhost:3000/todos/${task.id}`, {
+      isCompleted: isCompleted
+    });
+  }
   return (
-    <div className="todoItem">
-      <div className="buttons">
+    <>
+      <div
+        onClick={handleCompletion}
+        className={isCompleted ? "checked h1" : "todoItem"}
+      >
         <h1>{task.title}</h1>
-        <button>EDIT</button>
-        <button onClick = {deleteTodoHandler}>DELETE</button>
       </div>
-    </div>
+      <div className="buttons">
+        <button className="button" onClick={() => openModal(task.id)}>
+          EDIT
+        </button>
+        <button className="button" onClick={deleteTodoHandler}>
+          DELETE
+        </button>
+      </div>
+    </>
   );
 };
 
